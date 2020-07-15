f872249581e97aaffb4ac814e681499c63d966a5
Dmitry Zharikov <zdimon77@gmail.com>
2020-07-15 16:12:01 +0300

    commit from zdimon77@gmail.com ----

---
+++ pl/pl/_local.py
@@ -0,0 +1,9 @@
+DATA_DIR = ''
+VIDEO_DIR = '/home/zdimon/Videos/course-data/ruslan'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '769722970237-8lhn2anpmkjjagu1monnvooetln72i2b.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'eMiJChs6r6v7A6AoHY-YooaN'
+
+LESSON_PRICE = 50
+LIQPAY_PUBLIC_KEY   = '...'
+LIQPAY_PRIVATE_KEY  = '...'
+LIQPAY_PROCESS_URL  = 'https://learning.webmonstr.com/liqpay/process'
--- pl/pl/settings.py
+++ pl/pl/settings.py
@@ -129,10 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-DATA_DIR = str(os.path.join(BASE_DIR, "../data"))
-VIDEO_DIR = '/home/zdimon/Videos/course-data/ruslan'
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '769722970237-8lhn2anpmkjjagu1monnvooetln72i2b.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'eMiJChs6r6v7A6AoHY-YooaN'
+
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
 
 AUTHENTICATION_BACKENDS = (
@@ -143,8 +140,5 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-LESSON_PRICE = 50
 
-LIQPAY_PUBLIC_KEY   = 'sandbox_i44723543778'
-LIQPAY_PRIVATE_KEY  = 'sandbox_HYN5eC9cFYFxGkHsRX5wg8sEgxniYLPeONzfhmMv'
-LIQPAY_PROCESS_URL  = 'https://learning.webmonstr.com/liqpay/process'
+from .local import *
