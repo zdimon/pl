64415c4eb1293c18bfef79b86cf728838a24c876
Dmitry Zharikov <zdimon77@gmail.com>
2020-07-23 17:54:08 +0300

    commit from zdimon77@gmail.com ----

---
+++ pl/cabinet/management/commands/clear_promo.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+from cabinet.models import Promocode
+
+class Command(BaseCommand):
+  
+    def handle(self, *args, **options):
+        print('Clear promo')
+       
