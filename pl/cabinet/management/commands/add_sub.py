f11ee95bc68204839497ebb056e17b78663a8217
Dmitry Zharikov <zdimon77@gmail.com>
2020-09-14 17:52:15 +0300

    auto

---
+++ pl/cabinet/management/commands/add_sub.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from cabinet.models import UserProfile
+from course.models import Subscription
+
+class Command(BaseCommand):
+  
+    def handle(self, *args, **options):
+        
+        for u in UserProfile.objects.all():
+            print('add %s' % u.email)
+            s = Subscription()
+            s.email = u.email
+            s.is_subscribed = True
+            s.save()
+       
