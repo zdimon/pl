d0130f5b8da42ff51ab47a6dcafcec2bfc4e1c18
Dmitry Zharikov <zdimon77@gmail.com>
2020-10-03 17:08:57 +0300

    commit from zdimon77@gmail.com ----

--- frontend/angular/projects/core/src/lib/forms/order-form/order-form.component.ts
+++ frontend/angular/projects/core/src/lib/forms/order-form/order-form.component.ts
@@ -1,14 +1,19 @@
 
 
+
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { Validators, FormControl } from '@angular/forms';
 
 // Store
 import { Store } from '@ngrx/store';
 import { CategoryListState } from './../../store/states/category.state';
 import { selectCategoryArray, selectSubCategoriesByCategoryId } from './../../store/selectors/catalog.selector';
+
+// Services
+
+import { ApiService } from './../../services/api.service';
 
 
 @Component({
@@ -21,10 +26,12 @@ export class OrderFormComponent implements OnInit {
   form: any;
   categories: Observable<any>;
   subCategories: Observable<any>;
+  controls: any;
 
   constructor(
     private fb: FormBuilder,
-    private categoryStore: Store<CategoryListState>
+    private categoryStore: Store<CategoryListState>,
+    private api: ApiService
     ) {
       this.categories = this.categoryStore.select(selectCategoryArray);
   }
@@ -40,10 +47,24 @@ export class OrderFormComponent implements OnInit {
 
   changeCategory(event) {
     this.subCategories = this.categoryStore.select(selectSubCategoriesByCategoryId(event.value));
+    this.api.getFormFields(event.value).subscribe((rez: any) => {
+      this.buildForm(rez);
+      this.controls = rez.results;
+    });
   }
 
   changeSubCategory(event) {
     console.log(event.value);
+  }
+
+  buildForm(data: any) {
+    console.log('Build form');
+    console.log(data);
+    for (let field in data.results) {
+      if (data.results.hasOwnProperty(field)) {
+        this.form.addControl(data.results[field].alias, new FormControl(''));
+      }
+    }
   }
 
 }
