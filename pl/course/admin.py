505d57d2d4f51c5c577b568f88b99dc3f634d14b
Your Name <you@example.com>
2020-11-05 12:08:39 +0300

    auto

--- pl/course/admin.py
+++ pl/course/admin.py
@@ -69,8 +69,9 @@ class LessonAdmin(admin.ModelAdmin):
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ['title', 'filename', 'course', 'lesson', 'video', 'has_video', 'is_youtube']
+    list_display = ['title', 'filename', 'course', 'lesson', 'video', 'has_video', 'is_youtube', 'order']
     list_filter = ['has_video']
+    list_editable = ['order']
 
 
 
